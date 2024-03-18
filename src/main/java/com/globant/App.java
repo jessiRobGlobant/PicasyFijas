00f93e006c29be5c9640c3885a1fd516effd0ef0
Jessica Robles <jessica.robles@globant.com>
2024-03-18 15:23:11 -0500

    Added functional programming
    Added functional programing while sorting lists

--- src/main/java/com/globant/App.java
+++ src/main/java/com/globant/App.java
@@ -1,15 +1,52 @@
 package com.globant;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import com.globant.gameInterface.GameInterface;
+
+import lombok.extern.java.Log;
 
 /**
  * Hello world!
  *
  */
+@Log
 public class App 
 {
+    static List<String> names;
+
+    static{
+        names = Arrays.asList("John", "Jane", "Adam", "Eve", "William", "1", "5");
+        logInfo("Original list values");
+        names.forEach(name -> logInfo(name));
+    }
+
+    public static void lambdaEspresions (){
+
+        logInfo("Sorted ascendant");
+        names = names.stream().sorted().collect(Collectors.toList());
+        logInfo(names.toString());
+
+        logInfo("Sorted descendant");
+        names = names.stream().sorted(Collections.reverseOrder()).collect(Collectors.toList());
+        logInfo(names.toString());
+
+        logInfo("Sorted normally");
+        names.sort(null);
+        logInfo(names.toString());
+    }
     public static void main( String[] args )
     {
-        final GameInterface gameInterface = new GameInterface();
+        // final GameInterface gameInterface = new GameInterface();
+        lambdaEspresions();
+    }
+
+    public static void logInfo(String msg){
+        log.info(msg);
     }
 }
